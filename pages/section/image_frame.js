8e8e94bf03343e8b1ca5971c4aff7b0b8a30f888
zhangyb3 <cheung29@foxmail.com>
2017-04-24 11:26:14 +0800

    2017-04-24 1125

--- pages/section/image_frame.js
+++ pages/section/image_frame.js
@@ -1,127 +1,105 @@
-var olddistance = 0;  //这个是上一次两个手指的距离
-var newdistance;      //本次两手指之间的距离，两个一减咱们就知道了滑动了多少，以及放大还是缩小（正负嘛）
-var oldscale = 1;     //这个是上一次动作留下的比例
-var diffdistance;     //这个是新的比例，新的比例一定是建立在旧的比例上面的，给人一种连续的假象
-var baseHeight;       //上一次触摸完之后的高
-var baseWidth;        //上一次触摸完之后的宽
-var windowWidth = 0;  //咱们屏幕的宽
-var windowHeight = 0; //咱们屏幕的高
+//scale.js
+//获取应用实例
 
 Page({
-  data:{
-    scaleWidth:"",
-    scaleHeight:"",
-    dataimg:"",
+  data: {
+    stv: {
+      offsetX: 0,
+      offsetY: 0,
+      zoom: false, //是否缩放状态
+      distance: 0,  //两指距离
+      scale: 1,  //缩放倍数
+    }
   },
-  onLoad:function(parameters){
-    
-    console.log(parameters.image_source);
+  //事件处理函数
+  touchstartCallback: function(e) {
+    //触摸开始
+    console.log('touchstartCallback');
+    console.log(e);
 
-    var res = wx.getSystemInfoSync();  //获取系统信息的同步方法，我用了异步里面提示我this.setData错了
-    windowWidth = res.windowWidth;     
-    windowHeight = res.windowHeight;
-    //那就给前面的图片进行赋值，高，宽以及路劲 
-    this.setData({ 
-      scaleHeight:windowHeight,    
-      dataimg:decodeURIComponent(parameters.image_source),
-      scaleWidth:windowWidth, 
-    });
-
-  },
-
-  //这里是图片加载完毕之后的信息，因为滑动手指距离会变，我们要跟着图片的长宽进行缩放，不能跟着屏幕的长宽进行缩放
-  imgload:function(e){
-      var originalWidth = e.detail.width;//图片原始宽
-      var originalHeight = e.detail.height;//图片原始高
-      var originalScale = originalHeight/originalWidth;//图片高宽比
-      var windowscale = windowHeight/windowWidth;//屏幕高宽比
-      if(originalScale < windowscale){//图片高宽比小于屏幕高宽比
-        //图片缩放后的宽为屏幕宽
-          baseWidth = windowWidth;
-      
-          baseHeight = (windowWidth * originalHeight) / originalWidth;
-      }else{//图片高宽比大于屏幕高宽比
-        //图片缩放后的高为屏幕高
-         baseHeight = windowHeight;
-         baseWidth = (windowHeight * originalWidth) / originalHeight;
-      }
-  },
-
-  //两手指进行拖动了
-  movetap:function(event){
-     var e = event;
-     if(e.touches.length == 2)
-     {
-       var xMove = e.touches[1].clientX - e.touches[0].clientX;
-       var yMove = e.touches[1].clientY - e.touches[0].clientY;
-       var distance = Math.sqrt(xMove * xMove + yMove * yMove);//两手指之间的距离 
-       if (olddistance == 0)
-       {
-          olddistance = distance; //要是第一次就给他弄上值，什么都不操作
-          console.log(olddistance); 
-       }
-       else
-       {
-          newdistance = distance; //第二次就可以计算它们的差值了
-          diffdistance = newdistance - olddistance;
-          olddistance = newdistance; //计算之后更新
-          console.log(diffdistance);
-          var newScale = oldscale + 0.005 * diffdistance;  //比例
-          console.log(newScale);
-          //刷新.wxml
-          this.setData({ 
-           scaleHeight:newScale * baseHeight, 
-           scaleWidth:newScale * baseWidth 
-           
-          })     
-          oldscale = newScale;
-          //更新比例
-
-       }
-     }
-  },
-
-  endtap:function(event){
-    console.log(event);//抬起手指，保存下数据
-    if(event.touches.length == 2)
-    {
-      olddistance = 0;
+    if (e.touches.length === 1) {
+      let {clientX, clientY} = e.touches[0];
+      this.startX = clientX;
+      this.startY = clientY;
+      this.touchStartEvent = e.touches;
+    } else {
+      let xMove = e.touches[1].clientX - e.touches[0].clientX;
+      let yMove = e.touches[1].clientY - e.touches[0].clientY;
+      let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+      this.setData({
+        'stv.distance': distance,
+        'stv.zoom': true, //缩放状态
+      })
     }
 
   },
+  touchmoveCallback: function(e) {
+    //触摸移动中
+    //console.log('touchmoveCallback');
+    //console.log(e);
 
-
-
-
-  returnLastPage:function(e){
-    wx.navigateBack({
-      delta: 1, // 回退前 delta(默认为1) 页面
-      success: function(res){
-        // success
-      },
-      fail: function(res) {
-        // fail
-      },
-      complete: function(res) {
-        // complete
+    if (e.touches.length === 1) {
+      //单指移动
+      if (this.data.stv.zoom) {
+        //缩放状态，不处理单指
+        return ;
       }
-    });
-  },
+      let {clientX, clientY} = e.touches[0];
+      let offsetX = clientX - this.startX;
+      let offsetY = clientY- this.startY;
+      this.startX = clientX;
+      this.startY = clientY;
+      let {stv} = this.data;
+      stv.offsetX += offsetX;
+      stv.offsetY += offsetY;
+      stv.offsetLeftX = -stv.offsetX;
+      stv.offsetLeftY = -stv.offsetLeftY;
+      this.setData({
+        stv: stv
+      });
 
-  onReady:function(){
-    // 页面渲染完成
-    
+    } else {
+      //双指缩放
+      let xMove = e.touches[1].clientX - e.touches[0].clientX;
+      let yMove = e.touches[1].clientY - e.touches[0].clientY;
+      let distance = Math.sqrt(xMove * xMove + yMove * yMove);
+
+      let distanceDiff = distance - this.data.stv.distance;
+      let newScale = this.data.stv.scale + 0.005 * distanceDiff;
+
+      this.setData({
+        'stv.distance': distance,
+        'stv.scale': newScale,
+      })
+    }
+
   },
-  onShow:function(){
-    // 页面显示
-    
+  touchendCallback: function(e) {
+    //触摸结束
+    console.log('touchendCallback');
+    console.log(e);
+
+    if (e.touches.length === 0) {
+      this.setData({
+        'stv.zoom': false, //重置缩放状态
+      })
+    }
   },
-  onHide:function(){
-    // 页面隐藏
-    
-  },
-  onUnload:function(){
-    // 页面关闭
+  onLoad: function (parameters) {
+    console.log('onLoad');
+
+    var res = wx.getSystemInfoSync();  //获取系统信息的同步方法
+    console.log(res);
+    var windowWidth = res.windowWidth;     
+    var windowHeight = res.windowHeight;
+
+
+    console.log(parameters.image_source);
+    this.setData({
+      scaleHeight:windowHeight,       
+      scaleWidth:windowWidth,      
+      image_source: decodeURIComponent(parameters.image_source),
+    });
     
   }
 })
