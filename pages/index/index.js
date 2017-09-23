6efa77b0699a3f20095894d583bbac0f61dc92fc
zhangyb3 <cheung29@foxmail.com>
2017-09-23 17:08:03 +0800

    2017-09-23 1710

--- pages/index/index.js
+++ pages/index/index.js
@@ -57,6 +57,43 @@ Page({
         infoNum: 0,
       },
     ],
+
+    hide_login:true,
+
+    grades: [
+      {
+        gradeId: null,
+        grade: '请选择',
+      }, 
+      
+      
+    ],
+    grade_index:0,
+
+    subjects: [
+      {
+        subjectId: null,
+        subject: '请选择',
+      }, 
+      
+      
+    ],
+    subject_index:0,
+
+    hide_subject_selection:true,
+    hide_grade_selection:false,
+    selectStudent:true,
+    selectTeacher:false,
+
+    registerParams: {
+      status: null,
+      gradeId: null,
+      subjectId: null,
+    },
+
+    countdownText : '发送验证码',
+    second: 60,
+    
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
@@ -124,7 +161,7 @@ Page({
               {
                 wx.setStorageSync('alreadyRegister', 'no');
                 console.log("register : " + wx.getStorageSync('alreadyRegister'));
-                //注册
+                
                 
               }
               else
@@ -142,61 +179,21 @@ Page({
                 wx.setStorageSync(user.StudentID, registerInfo.userid);
                 wx.setStorageSync(user.TeacherID, registerInfo.userid);
 
-                // if(wx.getStorageSync('avatarUrl') != wx.getStorageSync('userAvatarUrl'))
-                // {
-                //   wx.request({
-                //     url: config.PytheRestfulServerURL + '/user/updateAvatar',
-                //     data: {
-                //       userId: wx.getStorageSync(user.UserID),
-                //       avatar: wx.getStorageSync('avatarUrl'),
-                //     },
-                //     method: 'GET', 
-                //     success: function(res){
-                //       // success
-                //       wx.setStorageSync('userAvatarUrl', wx.getStorageSync('avatarUrl'));
-                //     },
-                //     fail: function(res) {
-                //       // fail
-                //     },
-                //     complete: function(res) {
-                //       // complete
-                //     }
-                //   })
-                // }
+                
 
                 if(wx.getStorageSync(user.UserID)!='userID')
                 {
                   wx.setStorageSync('alreadyRegister', 'yes');
                   wx.setStorageSync('fromRegister', 'no');
                   
-                  var simple_params = {
-                    gradeId : wx.getStorageSync(user.GradeID),
-                    pageSize : 10,
-                    pageNum : 1,
-                    
-                  };
-                  this.setData({
-                    hide_loading: false,
+                  wx.showToast({
+                    title: '已登录',
+                    duration: 1200
                   });
-                  listViewUtil.loadList(that,'index',config.PytheRestfulServerURL,
-                  "/index/defaultList",
-                  10,
-                      simple_params,
-                      function (netData){
-                        //取出返回结果的列表
-                        return netData.data;
-                      },
-                      function(item,that){
-                        that.setData({
-                          hide_loading: true,
-                        });
-                      },
-                      {},
-                      'GET',
-                  );
-                  wx.setStorageSync('index_load_type', 'one');
-
+                  
                 }
+                
+
                 if(wx.getStorageSync(user.Status) == 1)
                 {
                   wx.setStorageSync(user.OrganizationID, registerInfo.organizationid);
@@ -212,78 +209,8 @@ Page({
         }
       );
     }
-    else
-    {
-      var simple_params = {
-        gradeId : wx.getStorageSync(user.GradeID),
-        pageSize : 10,
-        pageNum : 1,
-        
-      };
-      this.setData({
-        hide_loading: false,
-      });
-      listViewUtil.loadList(that,'index',config.PytheRestfulServerURL,
-      "/index/defaultList",
-      10,
-          simple_params,
-          function (netData){
-            //取出返回结果的列表
-            return netData.data;
-          },
-          function(item,that){
-            that.setData({
-              hide_loading: true,
-            });
-          },
-          {},
-          'GET',
-      );
-      wx.setStorageSync('index_load_type', 'one');
-    }
-
-
-
-    // this.setData({
-    //   alreadyRegister: wx.getStorageSync('alreadyRegister'),
-    // });
-
-    // var that = this;
-    // var simple_params = {
-    //   gradeId : wx.getStorageSync(user.GradeID),
-    //   pageSize : 10,
-    //   pageNum : 1,
-      
-    // };
-    // this.setData({
-    //   hide_loading: false,
-    // });
-    // listViewUtil.loadList(that,'index',config.PytheRestfulServerURL,
-    // "/index/defaultList",
-    // 10,
-    //     simple_params,
-    //     function (netData){
-    //       //取出返回结果的列表
-    //       return netData.data;
-    //     },
-    //     function(item,that){
-    //       that.setData({
-    //         hide_loading: true,
-    //       });
-    //     },
-    //     {},
-    //     'GET',
-    // );
-    // wx.setStorageSync('index_load_type', 'one');
     
 
-    // //判断是否从注册页面返回
-    // if(wx.getStorageSync('fromRegister')=='yes')
-    // {
-      
-
-    //   wx.setStorageSync('fromRegister', 'no');
-    // }
 
     this.setData({
       hide_register_lock_cover: true,
@@ -425,18 +352,42 @@ Page({
       }
       case '设置':
       {
-        wx.navigateTo({
-          url: '../personal/personal',
-          success: function(res){
-            // success
-          },
-          fail: function(res) {
-            // fail
-          },
-          complete: function(res) {
-            // complete
-          }
-        });
+        if(wx.getStorageSync('alreadyRegister') == 'no')
+        {
+          wx.navigateTo({
+            url: '../personal/personal',
+            success: function(res){
+              // success
+            },
+            fail: function(res) {
+              // fail
+            },
+            complete: function(res) {
+              // complete
+            }
+          });
+        }
+        else
+        {
+          // wx.showModal({
+          //   content: '尚未登录',
+          //   success: function(res) {
+          //     if (res.confirm) {
+          //       console.log('用户点击确定')
+          //     }
+          //   }
+          // });
+
+          //注册
+          this.setData({
+            hide_login: false,
+          });
+
+          loadingSelections.call(this);
+          
+
+        }
+        
         break;
       }
     }
@@ -468,6 +419,194 @@ Page({
 
   },
 
+  selectStudent:function(e){
+    console.log("学生");
+    this.setData({
+      hide_subject_selection:true,
+      hide_grade_selection:false,
+      select_student:true,
+      select_teacher:false
+    })
+
+    this.data.registerParams.status = 0;
+  },
+  selectTeacher:function(e){
+    console.log("老师");
+    this.setData({
+      hide_subject_selection:false,
+      hide_grade_selection:true,
+      select_student:false,
+      select_teacher:true
+    })
+
+    this.data.registerParams.status = 1;
+    this.data.registerParams.gradeId = 332;
+  },
+
+  gradeChange: function(e) {
+    console.log('年级', this.data.grades[e.detail.value])
+    this.setData({
+      grade_index: e.detail.value      
+    })
+
+    this.data.registerParams.gradeId = this.data.grades[e.detail.value].gradeid;
+  },
+  subjectChange: function(e) {
+    console.log('科目', this.data.subjects[e.detail.value])
+    this.setData({
+      subject_index: e.detail.value
+    })
+
+    this.data.registerParams.subjectId = this.data.subjects[e.detail.value].subjectid;
+  },
+
+  //注册
+  phoneNumberInput: function(e) {
+    var registerPhoneNum = e.detail.value;
+      console.log(e.detail.value);
+      wx.setStorageSync('registerPhoneNum', registerPhoneNum);
+  },
+
+  sendVerificationCode:function(res) {
+    console.log(wx.getStorageSync('registerPhoneNum'));
+    register.sendVerificationCode(wx.getStorageSync('registerPhoneNum'));
+
+    //重发倒数
+    var that = this;
+    
+    that.setData({  
+      second: 60,  
+      lock_countdown: true,
+      }); 
+    countdown(that);
+    if (second < 0) {  
+      that.setData({  
+        countdownText: "重发验证码"  ,
+        lock_countdown: false,
+      });  
+    }
+  },
+
+  verificationCodeInput: function(e) {
+    var verificationCode = e.detail.value;
+    console.log(e.detail.value);
+    wx.setStorageSync('verificationCode', verificationCode);
+  },
+
+  registerToMainPage:function(e){
+    var that = this;
+    if(that.data.registerParams.status == 0 && that.data.registerParams.gradeId == null)
+    {
+      wx.showModal({
+        title: '提示',
+        content: '年级必填',
+        success: function(res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      });
+    }
+    else if( that.data.registerParams.status == 1 && that.data.registerParams.subjectId == null)
+    {
+      wx.showModal({
+        title: '提示',
+        content: '科目必填',
+        success: function(res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+          }
+        }
+      });
+    }
+    else if((that.data.registerParams.status == 1 && that.data.registerParams.subjectId != null) || (that.data.registerParams.status == 0 && that.data.registerParams.gradeId != null))
+    {
+      //注册
+      that.setData({  
+        lock_register: true,
+      });
+
+      wx.request({
+        url: config.PytheRestfulServerURL + '/user/register/',
+        data: {
+          status:this.data.registerParams.status,
+          userName: wx.getStorageSync('wxNickName'),
+          phoneNum: wx.getStorageSync('registerPhoneNum'),
+          verificationCode: wx.getStorageSync('verificationCode'),
+          gradeId: this.data.registerParams.gradeId,
+          subjectId: this.data.registerParams.subjectId,
+          openId: wx.getStorageSync(user.OpenID),
+          userImg: wx.getStorageSync('userAvatarUrl'),
+        },
+        method: 'POST',
+        success: function(res){
+          // success
+          console.log(res);
+          if(res.data.data==null)
+          {
+            wx.showModal({
+              title: '提示',
+              content: res.data.msg,
+              success: function(res) {
+                if (res.confirm) {
+                  console.log('用户点击确定')
+                }
+              }
+            });
+            
+            that.setData({  
+              lock_register: false,
+            });
+          }
+          else if(res.data.data.userid != null)
+          {
+            wx.setStorageSync(user.UserID, res.data.data.userid);
+            wx.setStorageSync(user.StudentID, res.data.data.studentid);
+            wx.setStorageSync(user.TeacherID, res.data.data.teacherid);
+            wx.setStorageSync(user.GradeID, res.data.data.gradeid);
+            wx.setStorageSync(user.UserNickName, res.data.data.username);
+            wx.setStorageSync(user.UserDescription, res.data.data.description);
+
+            //判断注册是否成功，成功则返回index页面
+            wx.setStorageSync('alreadyRegister', 'yes');
+            that.setData({
+              hide_login:true,
+            });
+            that.setData({
+              userAvatarUrl: wx.getStorageSync('userAvatarUrl'),
+              userNickName: wx.getStorageSync(user.UserNickName),
+              userDescription: wx.getStorageSync(user.UserDescription),
+            });
+          }
+          
+        },
+        fail: function() {
+          // fail
+          wx.showModal({
+            title: '提示',
+            content: '登录失败，请重试',
+            success: function(res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+              }
+            }
+          });
+
+          that.setData({  
+            lock_register: false,
+          });
+        },
+        
+      });
+    }
+  },
+
+  cancelRegister:function(e){
+    this.setData({
+      hide_login: true,
+    });
+  },
+
   onHide:function(){
     // 页面隐藏
   },
@@ -475,3 +614,109 @@ Page({
     // 页面关闭
   }
 })
+
+//注册准备
+function loadingSelections() {
+  console.log('load picker dataset');
+
+  this.data.registerParams.gradeId = null;
+  this.data.registerParams.subjectId = null;
+
+
+  // 页面初始化 options为页面跳转所带来的参数
+  this.setData({
+    hide_subject_selection:true,
+    hide_grade_selection:false,
+    select_student:true,
+    select_teacher:false
+  });
+  this.data.registerParams.status = 0;
+
+  var that = this;
+
+  var subjectRange = ['请选择'];
+  //加载动态课程列表,年级列表
+  wx.request({
+    url: config.PytheRestfulServerURL + '/user/register/subject',
+    data: {
+
+    },
+    method: 'GET', 
+    success: function(res){
+      // success
+      console.log(res.data.data);
+      
+      for(var count = 0; count < res.data.data.data.length; count++)
+      {
+        
+        subjectRange[count+1] = res.data.data.data[count].subject;
+        that.data.subjects[count+1] = res.data.data.data[count];
+        console.log(subjectRange);
+      }
+      
+      that.setData({
+        subjectRange: subjectRange,
+      });
+    },
+    fail: function() {
+      // fail
+    },
+    complete: function() {
+      // complete
+    }
+  });
+
+  var gradeRange = ['请选择'];
+  //加载动态课程列表,年级列表
+  wx.request({
+    url: config.PytheRestfulServerURL + '/user/register/grade',
+    data: {
+
+    },
+    method: 'GET', 
+    success: function(res){
+      // success
+      console.log(res.data.data);
+      
+      for(var count = 0; count < res.data.data.data.length; count++)
+      {
+        
+        gradeRange[count+1] = res.data.data.data[count].gradename;
+        that.data.grades[count+1] = res.data.data.data[count];
+        console.log(gradeRange);
+      }
+      
+      that.setData({
+        gradeRange: gradeRange,
+      });
+    },
+    fail: function() {
+      // fail
+    },
+    complete: function() {
+      // complete
+    }
+  });
+}
+
+function countdown(that) {
+  var second = that.data.second ;
+    if (second < 0) {  
+      that.setData({  
+        countdownText: "重发验证码" ,
+        lock_countdown: false,
+      });  
+      return ;  
+  } 
+
+  var time = setTimeout(function(){  
+    that.setData({  
+      
+      countdownText: second + '秒后可重发',
+      second: second - 1  ,
+      lock_countdown: true,
+    });  
+    countdown(that);  
+    }  
+    ,1000)  
+}
